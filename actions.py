1d809a7833576ec8b03e8fa58458c7eb1528ed91
Patrick <patrick.r.daly@gmail.com>
2019-06-18 19:15:09 -0400

    add more restaurant model

--- actions.py
+++ actions.py
@@ -91,3 +91,65 @@ class ActionRestaurant(FormAction):
     def submit(self, dispatcher, tracker, domain):
         dispatcher.utter_template('utter_finish')
         return []
+
+
+class ActionHotel(FormAction):
+
+    REQUIRED_FIELDS = [
+        EntityFormField("people", "people"),
+        EntityFormField("location", "location"),
+        EntityFormField("price", "price"),
+        EntityFormField("date", "start_date"),
+        EntityFormField("date", "end_date")
+    ]
+
+    OPTIONAL_FIELDS = [
+        EntityFormField("has_gym", "has_gym"),
+        EntityFormField("has_spa", "has_spa"),
+        EntityFormField("breakfast", "breakfast")
+    ]
+
+    def name(self):
+        return "action_hotel"
+
+    def submit(self, dispatcher, tracker, domain):
+        dispatcher.utter_template('utter_finish')
+        return []
+
+
+class ActionSearchRestaurant(Action):
+
+    def name(self):
+        return "action_search_restaurant"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template('utter_search')
+        restaurant_api = RestaurantAPI()
+        restaurant = restaurant_api.search(tracker.get_slot('cuisine'))
+
+        return [SlotSet('restaurant', restaurant)]
+
+
+class ActionSearchHotel(Action):
+
+    def name(self):
+        return "action_search_hotel"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template('utter_search')
+        hotel_api = HotelAPI()
+        hotel = hotel_api.search(tracker.get_slot('location'))
+
+        return [SlotSet('hotel', hotel)]
+
+
+class ActionExplain(Action):
+
+    def name(self):
+        return "action_explain"
+
+    def run(self, dispatcher, tracker, domain):
+        # TODO: actually implement this
+        dispatcher.utter_message('This means blabla')
+
+        return []
